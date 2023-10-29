852b4a15a27ba072466f2cb5800857bc1ed778d0
lohith <lohithreddy.e@gmail.com>
2023-10-29 17:04:13 +0530

    more use cases and guidelines and internal workings of state

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -9,41 +9,81 @@ const messages = [
 ];
 
 const App = () => {
+  return (
+    <>
+      <Steps />
+      {/* <Counter />
+      <Counter /> */}
+    </>
+  );
+};
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((c) => c + 1);
+  };
+  const handleDecrement = () => {
+    setCount((c) => c - 1);
+  };
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+};
+
+const Steps = () => {
   // Default Value
-  // A function to update our state value
+  // A setter function to update our state value
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   const handlePrevious = () => {
     if (step <= 1) return;
-    setStep(step - 1);
+    // setStep(step - 1);
+    setStep((s) => s - 1);
   };
 
   const handleNext = () => {
     if (step === messages.length) return;
-    setStep(step + 1);
+    // setStep(step + 1);
+    // setStep(step + 1);
+    setStep((s) => s + 1);
+    // setStep((s) => s + 1);
   };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
-      <div className="buttons">
-        <button style={btnStyle} onClick={handlePrevious}>
-          Previous
-        </button>
-        <button style={btnStyle} onClick={handleNext}>
-          Next
-        </button>
-      </div>
-    </div>
+          <div className="buttons">
+            <button style={btnStyle} onClick={handlePrevious}>
+              Previous
+            </button>
+            <button style={btnStyle} onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
