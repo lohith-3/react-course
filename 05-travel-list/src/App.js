269ef7cbad2a96df51d1bec778080e9fd94f1f97
lohith <lohithreddy.e@gmail.com>
2023-12-26 17:55:05 +0530

    added logic for adding packing items

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,8 +1,119 @@
+import { useState } from "react";
+
+const initialItems = [
+  {
+    id: 1,
+    description: "Passports",
+    quantity: 2,
+    packed: false,
+  },
+  {
+    id: 2,
+    description: "Snacks",
+    quantity: 12,
+    packed: false,
+  },
+  {
+    id: 3,
+    description: "Charger",
+    quantity: 3,
+    packed: true,
+  },
+];
+
 const App = () => {
+  // const [items, setItems] = useState(initialItems);
   return (
-    <div>
-      <h1>Travel List App</h1>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackagingList />
+      <Stats />
     </div>
+  );
+};
+
+const Logo = () => {
+  return (
+    <>
+      <h1>🌴 Far Away 💼</h1>
+    </>
+  );
+};
+
+const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!description.replace(/\s/g, "")) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  };
+
+  console.log(quantity);
+
+  // onSubmit={(e)=> handleSubmit(e)}
+  // Value is only - Read Only
+  // So to basically update the state each time you change the
+  // quantity or text value you use on change event to update the
+  // state value
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item...."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+};
+
+const PackagingList = () => {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <span>❌</span>
+    </li>
+  );
+};
+
+const Stats = () => {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
   );
 };
 
