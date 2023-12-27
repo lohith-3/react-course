29083e1c9b1befd6fed480226caef9ef2ea4b2dd
lohith <lohithreddy.e@gmail.com>
2023-12-27 17:01:15 +0530

    moving components to its own folder

---
+++ 05-travel-list/src/components/Form.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const Form = ({ onAddItems }) => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!description.replace(/\s/g, "")) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  };
+
+  // console.log(items);
+
+  // onSubmit={(e)=> handleSubmit(e)}
+  // Value is only - Read Only
+  // So to basically update the state each time you change the
+  // quantity or text value you use on change event to update the
+  // state value
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item...."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+};
+
+export default Form;
---
+++ 05-travel-list/src/components/PackingList.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Item from "./Item";
+
+const PackagingList = ({ items, onDeleteItem, onUpdateItem, onClearItems }) => {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.length > 0 &&
+          sortedItems.map((item) => (
+            <Item
+              item={item}
+              key={item.id}
+              onDeleteItem={onDeleteItem}
+              onUpdateItem={onUpdateItem}
+            />
+          ))}
+      </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed Status</option>
+        </select>
+        <button onClick={onClearItems}>Clear List</button>
+      </div>
+    </div>
+  );
+};
+
+export default PackagingList;
