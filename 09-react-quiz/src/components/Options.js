93415d0ec7331f57702780b263b4e9525e25964f
lohith <lohithreddy.e@gmail.com>
2024-03-22 14:29:57 +0530

    completed react quiz with usereducer hook implementation

---
+++ 09-react-quiz/src/components/Options.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+  return (
+    <div className="options">
+      {question.options.map((option, idx) => (
+        <button
+          className={`btn btn-option ${idx === answer ? "answer" : ""} ${
+            hasAnswered
+              ? idx === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: idx })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Options;
